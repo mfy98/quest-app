421f256b34f5c9fb64523d63b8dab5e46068ca23
mfy98 <mfurkanyigit98@gmail.com>
2023-09-17 01:15:39 +0300

    "Like" controller and service added.

--- src/main/java/com/filxdev/questapp/services/LikeService.java
+++ src/main/java/com/filxdev/questapp/services/LikeService.java
@@ -1,9 +1,65 @@
 package com.filxdev.questapp.services;
 
-import com.filxdev.questapp.repos.LikeRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
+
+import com.filxdev.questapp.entities.Like;
+import com.filxdev.questapp.entities.Post;
+import com.filxdev.questapp.entities.User;
+import com.filxdev.questapp.repos.LikeRepository;
+import com.filxdev.questapp.requests.LikeCreateRequest;
+import com.filxdev.questapp.responses.LikeResponse;
 
 @Service
 public class LikeService {
-    LikeRepository likeRepository;
+
+    private final LikeRepository likeRepository;
+    private final UserService userService;
+    private final PostService postService;
+
+    public LikeService(LikeRepository likeRepository, UserService userService,
+                       PostService postService) {
+        this.likeRepository = likeRepository;
+        this.userService = userService;
+        this.postService = postService;
+    }
+
+    public List<LikeResponse> getAllLikesWithParam(Optional<Long> userId, Optional<Long> postId) {
+        List<Like> list;
+        if(userId.isPresent() && postId.isPresent()) {
+            list = likeRepository.findByUserIdAndPostId(userId.get(), postId.get());
+        }else if(userId.isPresent()) {
+            list = likeRepository.findByUserId(userId.get());
+        }else if(postId.isPresent()) {
+            list = likeRepository.findByPostId(postId.get());
+        }else
+            list = likeRepository.findAll();
+        return list.stream().map(LikeResponse::new).collect(Collectors.toList());
+    }
+
+    public Like getOneLikeById(Long LikeId) {
+        return likeRepository.findById(LikeId).orElse(null);
+    }
+
+    public Like createOneLike(LikeCreateRequest request) {
+        User user = userService.getOneUserById(request.getUserId());
+        Post post = postService.getOnePostById(request.getPostId());
+        if(user != null && post != null) {
+            Like likeToSave = new Like();
+            likeToSave.setId(request.getId());
+            likeToSave.setPost(post);
+            likeToSave.setUser(user);
+            return likeRepository.save(likeToSave);
+        }else
+            return null;
+    }
+
+    public void deleteOneLikeById(Long likeId) {
+        likeRepository.deleteById(likeId);
+    }
+
+
 }
