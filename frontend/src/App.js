10580799822964330a05f53b3d85f2907392a4ea
mfy98 <mfurkanyigit98@mail.com>
2023-09-20 00:29:55 +0300

    New components added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import Home from './components/Home/Home';
+import User from './components/User/User';
+import Navbar from  './components/Navbar/Navbar';
 import './App.css';
-
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar></Navbar>
+        <Switch>
+            <Route exact path="/" Component={Home}></Route>
+            <Route exact path="/users/:userId" Component={User}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
